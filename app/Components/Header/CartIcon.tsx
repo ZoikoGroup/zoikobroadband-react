3ec3045662c74420954cff4117fa5cd23c763252
sumonklyn <sumonklyn@gmail.com>
2026-05-07 15:37:38 +0530

    checkout 3

--- app/Components/Header/CartIcon.tsx
+++ app/Components/Header/CartIcon.tsx
@@ -18,7 +18,7 @@ const CartIcon = () => {
       <ShoppingCart className="w-6 h-6" />
 
       {cartCount > 0 && (
-        <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
+        <span className="absolute -top-2 -right-2 bg-[#10446c] text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
           {cartCount}
         </span>
       )}
